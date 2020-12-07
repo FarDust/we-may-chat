f5ad23ddd08b103d8b03b36c10dd89b0d9c48205
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-12-07 16:38:20 -0300

    build: add production databases settings

--- chat/settings.py
+++ chat/settings.py
@@ -99,14 +99,14 @@ WSGI_APPLICATION = 'chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
+}
+if not DEBUG:
     DATABASES["default"] = env.db("DATABASE_URL")
 
 
