2aeb4500a1f699b5aafb315a2f6cc76881162b2d
Gabriel Faundez <gabriel.faundez@gmail.com>
2021-04-19 17:48:48 -0400

    fix: change migration history

--- app/message/migrations/0001_initial.py
+++ app/message/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('content', models.TextField()),
-                ('timestamp', models.TimeField(auto_now=True)),
+                ('timestamp', models.DateTimeField(auto_now=True)),
                 ('session', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='sessions.Session')),
             ],
         ),
