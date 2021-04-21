a841f8cdc4dedac9f6d623ad632cc29284f3aec0
Gabriel Faundez <gabriel.faundez@gmail.com>
2021-04-21 14:10:47 -0400

    fix(heroku): dummy remastered

--- release.sh
+++ release.sh
@@ -1,7 +1,2 @@
 python manage.py migrate
-cd ./frontend/chat-room
-export NPM_CONFIG_PRODUCTION=false
-npm install
-npm run build-local
-cd ../../
 python manage.py collectstatic --dry-run --noinput
